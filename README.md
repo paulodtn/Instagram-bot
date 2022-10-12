95a3c8e1ac67c4561bada8e6ed87ca258c775919
Giovanna Bloise <giovanna.bloise@varsomics.com>
2022-10-12 11:12:52 -0300

    Adicionar pacote click e alterar documentacao

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Instagram-bot
-criando um mecanismo de automação para comentar e marcar em posts
+# InstaBot :robot: :computer:
+Projeto destinado a criação de automação para comentar e marcar em posts no Instagram.
+
+## Instalação
+
+Utilizar o arquivo `requirements.txt` para instalar o pacote necessário.
+
+```bash
+pip3 install -r requirements.txt
+```
+
+## Modo de uso
+Para fazer uso da ferramenta, deve-se fornecer a conta cadastrada no Instagram e sua senha. 
+
+| Parâmetro          | Descrição              |  Requisito |
+| ------------------ | ---------------------- | ---------------- |
+| --user             | Sua conta no Instagram |  obrigatório     |
+| --password_account | Sua senha no instagram |  obrigatório     |
+|                    |                        |                  |
+
+## Limitações
+-> Se a conta houver autenticação em dois fatores, a ferramenta não funcionará, pois identificará um acesso estranho.
--- bot_insta.py
+++ bot_insta.py
@@ -1,28 +1,34 @@
-
-#print(20*'\033[44m-','VAMOS PROGRAMAR',20*'\033[44m-\033[m')
-
-#navegador = webdriver.Firefox('D:\\VSCode\\Pyton\\BOT INSTA\\geckodriver.exe')
-
-#navegador.get('https://www.instagram.com/')
-
-
-#from selenium import webdriver
-#from selenium.webdriver.chrome.service import Service
-#from webdriver_manager.chrome import ChromeDriverManager
-
-#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-#driver.get("https://www.google.com")
-
-from time import sleep
-from unicodedata import name
+from numpy import require
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+import time
+import click
 
-options = Options()
-options.add_argument("start-maximized")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get("https://www.google.com")
-sleep(3)
-login = driver.find_element(By.NAME, "name")
+@click.command()
+@click.option("--user", help = "Your acount password", required=True)
+@click.option("--password_account", help = "Your acount password", required=True)
+@click.option("--page", help = "Page to comment", required=True )
+
+def instagram(user, password_account, page):
+    """Program to automate comments on instagram page"""
+    browser = webdriver.Firefox()
+    browser.get("https://www.instagram.com")
+
+    time.sleep(5)
+    username = browser.find_element("css selector", "input[name='username']")
+    password = browser.find_element("css selector", "input[name='password']")
+    username.clear()
+    password.clear()
+    username.send_keys(user)
+    password.send_keys(password_account)
+    login = browser.find_element("css selector", "button[type='submit']").click()
+
+    time.sleep(5)
+
+    browser.get(page)
+    text_area = browser.find_element("id", "textarea")
+    text_area.send_keys("@recifeordinário")
+
+
+if __name__ == '__main__':
+    instagram()
